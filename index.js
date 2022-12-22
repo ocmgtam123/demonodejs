8aa98f2476299e5fda0e9739033aff124b3a7d58
hoangtamau <hoangtamau@gmail.com>
2022-12-22 09:50:43 +0700

    upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+const server = require("http").createServer(app);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get("/", (req,res)=>{
+    res.send("tesst");
+});
+server.listen(port, (req, res) => {
+    console.log("Server is runing port: " + port);
+})
